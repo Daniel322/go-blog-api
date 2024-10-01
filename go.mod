1fcec6c2ab42266f315313e6693c8b08be6cd9b8
Daniil Kravchenko <daniilkravchenko@MacBook-Pro-Daniil.local>
2024-10-01 10:53:13 +0300

    init commit

---
+++ go.mod
@@ -0,0 +1,21 @@
+module test-server
+
+go 1.23.1
+
+require (
+	github.com/kalimatas/go-tour-ru v0.0.0-20200113210955-d96baa9d6d16 // indirect
+	github.com/labstack/echo/v4 v4.12.0 // indirect
+	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	github.com/yuin/goldmark v1.4.13 // indirect
+	golang.org/x/crypto v0.22.0 // indirect
+	golang.org/x/mod v0.8.0 // indirect
+	golang.org/x/net v0.24.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/tools v0.6.0 // indirect
+	golang.org/x/tour v0.1.0 // indirect
+)
---
+++ server.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/labstack/echo/v4"
+	"math/rand"
+	"net/http"
+	"strconv"
+)
+
+type User struct {
+	email    string
+	password string
+}
+
+type Todo struct {
+	Id     int    `json:"id"`
+	Todo   string `json:"todo"`
+	Status string `json:"status"`
+}
+
+type ResponseMessage struct {
+	Message string `json:"message"`
+	Status  string `json:"status"`
+}
+
+var mockUser = User{email: "test@test.com", password: "qwerty"}
+
+func main() {
+	var todoList []Todo
+	e := echo.New()
+	e.GET("/todos", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, json.NewEncoder(c.Response()).Encode(todoList))
+	})
+
+	e.POST("/todos", func(c echo.Context) error {
+		jsonBody := Todo{Id: rand.Intn(1000)}
+		err := json.NewDecoder(c.Request().Body).Decode(&jsonBody)
+		if err != nil {
+			fmt.Println(err)
+			return c.String(http.StatusBadRequest, "Error on JSON parsing")
+		}
+		todoList = append(todoList, jsonBody)
+		fmt.Println(todoList)
+		return c.JSON(http.StatusOK, json.NewEncoder(c.Response()).Encode(todoList))
+	})
+
+	e.DELETE("/todos/:id", func(c echo.Context) error {
+		idParam, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			return c.String(http.StatusBadRequest, "Error on parsing")
+		}
+
+		var indexOfDeletedTodo int
+
+		for i := 0; i < len(todoList); i++ {
+			if todoList[i].Id == idParam {
+				indexOfDeletedTodo = i
+				break
+			}
+		}
+
+		todoList = append(todoList[:indexOfDeletedTodo], todoList[indexOfDeletedTodo+1:]...)
+
+		return c.JSON(
+			http.StatusOK,
+			json.NewEncoder(c.Response()).Encode(
+				ResponseMessage{
+					Message: "todo with id " + c.Param("id") + " deleted",
+					Status:  "success",
+				},
+			),
+		)
+	})
+
+	e.PATCH("/todos/:id", func(c echo.Context) error {
+		idParam, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			return c.String(http.StatusBadRequest, "Error on parsing")
+		}
+
+		var jsonBody Todo
+		err = json.NewDecoder(c.Request().Body).Decode(&jsonBody)
+		if err != nil {
+			fmt.Println(err)
+			return c.String(http.StatusBadRequest, "Error on JSON parsing")
+		}
+
+		var indexOfUpdatedTodo int
+
+		for i := 0; i < len(todoList); i++ {
+			if todoList[i].Id == idParam {
+				indexOfUpdatedTodo = i
+				break
+			}
+		}
+
+		if jsonBody.Status != "" {
+			todoList[indexOfUpdatedTodo].Status = jsonBody.Status
+		}
+
+		if jsonBody.Todo != "" {
+			todoList[indexOfUpdatedTodo].Todo = jsonBody.Todo
+		}
+
+		return c.JSON(http.StatusOK, json.NewEncoder(c.Response()).Encode(todoList))
+	})
+	e.Logger.Fatal(e.Start(":1323"))
+}
